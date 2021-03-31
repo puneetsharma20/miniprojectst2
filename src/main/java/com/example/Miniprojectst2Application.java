372e12937b53f56a94814fdc7748c77963c9c97d
puneet <puneetrocks20@gmail.com>
2021-03-31 17:30:17 +0530

    st2 commit

---
+++ src/main/java/com/example/Miniprojectst2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Miniprojectst2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Miniprojectst2Application.class, args);
+	}
+
+}
